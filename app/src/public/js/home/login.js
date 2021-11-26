fc6213894debe2b3bc11c115ce4d213d68757b48
ErranderLee <dre12am14@gmail.com>
2021-11-26 17:24:47 +0900

    Saving html object using DOM

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,17 @@
 "use strict";
 
-console.log("hihihihihi");
-console.log("byubybybybyby");
+const id  = document.querySelector("#id");
+const psword = document.querySelector("#psword");
+const loginBtn = document.querySelector("button");
+// #은 id로 부여되어 있는 값을 가져오라는 것을 의미.
+
+loginBtn.addEventListener("click", login);
+// Click 이벤트 발생 시 콜백함수(login) 실행
+
+function login() {
+    const req = {
+        id: id.value,
+        psword: psword.value
+    };
+    console.log(req);
+};
