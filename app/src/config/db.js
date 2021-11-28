617c8437fd32167bef3d7d7a86e39db43190251d
ErranderLee <dre12am14@gmail.com>
2021-11-29 03:43:50 +0900

    db connection info deleted

--- app/src/config/db.js
+++ app/src/config/db.js
@@ -1,9 +1,9 @@
 const maria = require("mysql");
 
 const db = maria.createConnection({
-    host: "socialloginpractice.cxx37clovtid.ap-northeast-2.rds.amazonaws.com",
-    user: "hoyonglee",
-    password: "ajoulee1214",
+    host: "",
+    user: "",
+    password: "",
     database: "login_lecture",
 });
 
