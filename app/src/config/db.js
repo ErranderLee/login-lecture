958e9f99848c05c335d9da7e32df07b8deb69591
HoyongLee <85232033+ErranderLee@users.noreply.github.com>
2021-11-29 03:28:49 +0900

    Update db.js

--- app/src/config/db.js
+++ app/src/config/db.js
@@ -1,1 +1,12 @@
+const maria = require("mysql");
 
+const db = maria.createConnection({
+    host: "socialloginpractice.cxx37clovtid.ap-northeast-2.rds.amazonaws.com",
+    user: "hoyonglee",
+    password: "ajoulee1214",
+    database: "login_lecture",
+});
+
+db.connect();
+
+module.exports = db;
