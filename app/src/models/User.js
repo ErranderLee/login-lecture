6ec08731419be240c1f81454f818e4aa1bbb6ac2
ErranderLee <dre12am14@gmail.com>
2021-11-29 03:08:11 +0900

    maria db를 이용한 로그인과 회원가입 기능 구현

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -13,17 +13,22 @@ class User {
         // Promise가 비동기적으로 동작하기 때문에 데이터를 모두 읽기 전에 출력되서 undefined가 출력된다. 
         // 따라서 기다리라는 것을 명시해야 하는데 그것이 await이다. Promise를 반환하는 것에만 적용할 수 있다.
         // await는 async 함수 안에서만 쓸 수 있다.
-        const {id, psword} = await UserStorage.getUserInfo(this.body.id);
-        // 이 login 함수를 실행하는 것에도 async, await을 걸어줘야 한다.
-        if (id) {
-            if (id === this.body.id && psword === this.body.psword) {
-                return { success: true };
+        try {
+            const {id, psword} = await UserStorage.getUserInfo(this.body.id);
+            
+            // 이 login 함수를 실행하는 것에도 async, await을 걸어줘야 한다.
+            if (id) {
+                if (id === this.body.id && psword === this.body.psword) {
+                    return { success: true };
+                }
+                else {
+                    return { success: false, msg:"비밀번호가 틀렸습니다. "};
+                }
             }
-            else {
-                return { success: false, msg:"비밀번호가 틀렸습니다. "};
-            }
+            return { success: false, msg:"존재하지 않는 아이디 입니다. "};
+        } catch(err) {
+            return { success: false, msg: "존재하지 않는 아이디 입니다. " };
         }
-        return { success: false, msg:"존재하지 않는 아이디 입니다. "};
     }
 
     async register() {
--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,8 +1,10 @@
 "use strict";
 
-const { json } = require("body-parser");
-const fs = require("fs").promises;
+
+// const fs = require("fs").promises;
 // .promises를 붙임으로써 promise 객체를 반환함.
+
+const db = require("../config/db");
 
 class UserStorage {
     // static #users = {
@@ -12,62 +14,72 @@ class UserStorage {
     // }; // 클래스 내에 변수 선언할 때 const 필요없음.
     //    // static : class를 인스턴스화 시키지 않고 변수를 불러올 수 있음.
     //    // # : public 변수를 private으로 바꾼다 -> 은닉화
-    static #getUserInfo(data, id) {
-        const users = JSON.parse(data);
-        const idx = users.id.indexOf(id);
-        const userKeys = Object.keys(users); // => {id, psword, name}
-        // users의 key값을 list로 만든다.
-        const userInfo = userKeys.reduce((newUser, info) => {
-            newUser[info] = users[info][idx];
-            // newUser에 key(info)에 따른 값이 순차적으로 들어간다.
-            return newUser;
-        }, {});
+    // static #getUserInfo(data, id) {
+    //     const users = JSON.parse(data);
+    //     const idx = users.id.indexOf(id);
+    //     const userKeys = Object.keys(users); // => {id, psword, name}
+    //     // users의 key값을 list로 만든다.
+    //     const userInfo = userKeys.reduce((newUser, info) => {
+    //         newUser[info] = users[info][idx];
+    //         // newUser에 key(info)에 따른 값이 순차적으로 들어간다.
+    //         return newUser;
+    //     }, {});
 
-        return userInfo;
-    }
+    //     return userInfo;
+    // }
 
-    static #getUsers(data, isAll, fields) {
-        const users = JSON.parse(data); // 버퍼 데이터를 읽을 수 있는 데이터로 변환.
-        if (isAll) return users; // 모든 필드의 정보 return.
-        const newUsers = fields.reduce((newUsers, field) => {
-            if (users.hasOwnProperty(field)) {
-                newUsers[field] = users[field];
-            }
-            return newUsers;
-        }, {});
-        return newUsers;
-    }
+    // static #getUsers(data, isAll, fields) {
+    //     const users = JSON.parse(data); // 버퍼 데이터를 읽을 수 있는 데이터로 변환.
+    //     if (isAll) return users; // 모든 필드의 정보 return.
+    //     const newUsers = fields.reduce((newUsers, field) => {
+    //         if (users.hasOwnProperty(field)) {
+    //             newUsers[field] = users[field];
+    //         }
+    //         return newUsers;
+    //     }, {});
+    //     return newUsers;
+    // }
 
-    static getUsers(isAll, ...fields) {
-        return fs.readFile("./src/databases/hoyonglee/users.json") // promise 객체를 반환
-            .then((data) => {
-                return this.#getUsers(data, isAll, fields);
-            })
-            .catch((err) => console.log)
+    // static getUsers(isAll, ...fields) {
+    //     return fs.readFile("./src/databases/hoyonglee/users.json") // promise 객체를 반환
+    //         .then((data) => {
+    //             return this.#getUsers(data, isAll, fields);
+    //         })
+    //         .catch((err) => console.log)
         
-    } // static 변수를 return 하므로 static으로 만든다.
+    // } // static 변수를 return 하므로 static으로 만든다.
       // static으로 선언했으므로 클래스를 인스턴스화 하지 않아도 쓸 수 있다.
     static getUserInfo(id) {
-        return fs.readFile("./src/databases/hoyonglee/users.json") // promise 객체를 반환
-            .then((data) => {
-                return this.#getUserInfo(data, id);
-            })
-            .catch((err) => console.log)
+        return new Promise((resolve, reject) => {
+            db.query("SELECT * FROM users WHERE id = ?", [id], (err, data) => {
+                if (err) reject(`${err}`);
+                resolve(data[0]);
+            });
+        })
+        
     }
 
     static async save(userInfo) {
         // 모든 데이터를 불러와서 추가한 후 다시 저장.
-        const users = await this.getUsers(true);
+    //     const users = await this.getUsers(true);
         
-        if (users.id.includes(userInfo.id)) {
-            throw "이미 존재하는 아이디입니다.";
-            // 문자열을 error로 throw 할 수 있음.
-        }
-        users.id.push(userInfo.id);
-        users.name.push(userInfo.name);
-        users.psword.push(userInfo.psword);
-        fs.writeFile("./src/databases/hoyonglee/users.json", JSON.stringify(users));
-        return { success: true };
+    //     if (users.id.includes(userInfo.id)) {
+    //         throw "이미 존재하는 아이디입니다.";
+    //         // 문자열을 error로 throw 할 수 있음.
+    //     }
+    //     users.id.push(userInfo.id);
+    //     users.name.push(userInfo.name);
+    //     users.psword.push(userInfo.psword);
+    //     fs.writeFile("./src/databases/hoyonglee/users.json", JSON.stringify(users));
+    //     return { success: true };
+    return new Promise((resolve, reject) => {
+        db.query("INSERT INTO users(id, name, psword) VALUES(?, ?, ?)", 
+        [userInfo.id, userInfo.name, userInfo.psword], 
+        (err) => {
+            if (err) reject(`${err}`);
+            resolve({ success: true });
+        });
+    })
     }
 }
 
