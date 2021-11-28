cf4ecd73eccbf749d4ba24820c72176c93ef1db8
ErranderLee <dre12am14@gmail.com>
2021-11-28 17:00:25 +0900

    회원가입 요청 구현

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -2,20 +2,27 @@
 
 
 const id  = document.querySelector("#id");
+const name = document.querySelector("#name");
 const psword = document.querySelector("#psword");
-const loginBtn = document.querySelector("#button");
+const confirmPsword = document.querySelector("#confirm-psword");
+const registerBtn = document.querySelector("#button");
+
 // #은 id로 부여되어 있는 값을 가져오라는 것을 의미.
 
-loginBtn.addEventListener("click", login);
-// Click 이벤트 발생 시 콜백함수(login) 실행
+registerBtn.addEventListener("click", register);
+// Click 이벤트 발생 시 콜백함수(register) 실행
 
-function login() {
+function register() {
     const req = {
         id: id.value,
-        psword: psword.value
+        name: name.value,
+        psword: psword.value,
+        confirmPsword: confirmPsword.value,
     };
+
+    console.log(req);
     
-    fetch("/login", {
+    fetch("/register", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -7,6 +7,7 @@ const ctrl = require("./home.ctrl") // 컨트롤러를 받는다.
 
 router.get("/", ctrl.output.home);
 router.get("/login", ctrl.output.login);
+router.get("/register", ctrl.output.register);
 router.post("/login", ctrl.process.login);
 
 module.exports = router; // 라우터를 외부에서 사용할 수 있도록 내보내기.
